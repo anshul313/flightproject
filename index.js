93ef4e0426072b98485a67f066a852ab7459e55c
Vedant <iamvedantsomani@gmail.com>
2016-07-21 14:39:55 +0530

    Added checkin endpoint

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.post('/checkin',function(req,res){
 		var user1 = (chunk.from < chunk.to) ? chunk.from : chunk.to;
 		var user2 = (chunk.from < chunk.to) ? chunk.to : chunk.from;
         var initiator = chunk.from;
+        var receiver = chunk.to;
         var flight = chunk.flight_id;
         var flight_time = chunk.flight_time;
 
@@ -109,7 +110,54 @@ app.post('/checkin',function(req,res){
        	});
 
        	get_flight_number_req.write(get_flight_number_data);
-       	get_flight_number_req.end(); 
+       	get_flight_number_req.end();
+
+		var receiver_token = null;
+		var notification_data = '{"columns":["device_token","device_type"],"where":{"id":'+receiver+'}}';
+				
+		var notification_options = {
+			host : 'data.earthly58.hasura-app.io',
+			port : '80',
+			path : '/api/1/table/user/select',
+			method: 'POST',
+			headers : {
+				'Content-Type' : 'application/json',
+				'Content-Length': Buffer.byteLength(notification_data),
+				'Authorization' : 'Hasura '+auth_data.auth_token
+			}
+		};
+
+		var notification_req = httpm.request(notification_options,function(res){
+			res.setEncoding('utf8');
+			res.on('data',function(chunk){
+				chunk=JSON.parse(chunk);
+				receiver_token=chunk[0];
+
+				var message = {
+					to : receiver_token.device_token,
+					collapse_key : 'my_collapse_key',
+					data : {
+						from_user : user.from,
+						from_username : user.from_username,
+						type : "checkin_req"
+					}
+				};
+
+				if(receiver_token.device_type!="ios"){
+					fcm.send(message,function(err,res){
+						if(err){
+							console.log('err : ',err);
+							console.log('res : ',res);
+						} else {
+							console.log('Successfully sent notification with response : '+res+'to : '+receiver_token.device_token);
+						}
+					});	
+				}
+			});
+		});
+
+		notification_req.write(notification_data);
+		notification_req.end();
 	});
 	res.send("Successfully Executed !");
 });
