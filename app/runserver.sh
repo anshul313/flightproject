d82d1e74bdccf2a3ff178f15ee0490ec4aec6173
Tanmai Gopal <tanmaig@34cross.in>
2016-11-19 20:52:58 +0530

    removes keys from runserver.sh. wtf

--- app/runserver.sh
+++ app/runserver.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-API_TOKEN="7hm5fasz9kfn9hk8v01u06di128peixm" \
-FCM_KEY="AIzaSyAlEs8Uag-FVRJ-mSjqJIqZbg5x4vc5Tx0" \
-ANDROID_VERSION="1.0"  \
-
- npm run start-prod
-
-
+npm run start-prod
