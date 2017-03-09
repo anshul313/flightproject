a5fc146ea5b4f7e94c5998c7350e3f6a0c593290
anshul313 <anshul313>
2017-03-09 18:01:15 +0530

    development_authToken updated

--- app/hasuraconfig.js
+++ app/hasuraconfig.js
@@ -3,7 +3,5 @@ module.exports = {
   port: {
     development: 3000,
     production: 8080
-  },
-  ACCESS_KEY: "AKIAIDNABRSVLUSX6ESA",
-  SECRET_KEY: "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",
+  }
 };
